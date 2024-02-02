e6a8ba278a4ae7e3aaa52b9c56152b458f580d3c
Shreya Bhavsar <shreyabhavsar@Shreyas-Air.mshome.net>
2024-02-02 12:17:00 +0530

    Initial commit

---
+++ Clima-iOS13/Clima/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Shreya Bhavsar on 10/01/24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let id: Int
+}
---
+++ WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Shreya Bhavsar on 10/01/24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
